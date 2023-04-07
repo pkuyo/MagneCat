d0b7e0a27abbbb50c953aef2dda192c4beced071
Pkuyo <miku_mi@foxmail.com>
2023-04-07 13:12:33 +0800

    眩晕机制，取矛，部分微调

--- MagneCat/hook/FloatingSpearFeature.cs
+++ MagneCat/hook/FloatingSpearFeature.cs
@@ -4,11 +4,11 @@ using RWCustom;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MagneCat.hook
 {
@@ -18,9 +18,15 @@ namespace MagneCat.hook
         {
             On.Player.ctor += Player_ctor;
             On.Player.Update += Player_Update;
+            On.Player.Stun += Player_Stun;
         }
 
-     
+        private static void Player_Stun(On.Player.orig_Stun orig, Player self, int st)
+        {
+            orig(self, st);
+            if (floatingSpear.TryGetValue(self, out var module))
+                module.Stun(st);
+        }
 
         private static void Player_ctor(On.Player.orig_ctor orig, Player self, AbstractCreature abstractCreature, World world)
         {
@@ -55,10 +61,10 @@ namespace MagneCat.hook
             spearList = new List<int>();
             floatingCore = new FloatingCore(this);
 
-            SpearState = FloatingSpearState.Float;
+            SpearState = FloatingSpearState.Suction;
         }
 
-        public Spear GetSpear()
+        public Spear? GetSpear()
         {
             Player player;
             if (!playerRef.TryGetTarget(out player) || spearList.Count == 0)
@@ -78,20 +84,17 @@ namespace MagneCat.hook
             spear.RemoveFromRoom();
             spearList.Add((spear is ExplosiveSpear)?1 : (spear is ElectricSpear) ? 2 : 0);
             spear.Destroy();
-            Debug.Log("Store Spear! Count :" + spearList.Count);
         }
 
 
         public void Update()
         {
-            Debug.Log("update");
             Player player;
             if (!playerRef.TryGetTarget(out player))
                 return;
             switch (SpearState)
             {
                 case FloatingSpearState.Float:
-                    //TODO : 漂浮模式！
                     if(player.room != null && !player.inShortcut)
                     {
                         floatingCore.Update();
@@ -100,14 +103,16 @@ namespace MagneCat.hook
                     break;
                     
                 case FloatingSpearState.Suction:
-                    if (player.room!=null)
+                    if (stunCoolDownCounter > 0)
+                        stunCoolDownCounter--;
+                    if (player.room!=null && stunCoolDownCounter == 0)
                     {
                         foreach(var physicals in player.room.physicalObjects)
                         {
                             foreach(var phyObject in physicals)
                             {
                                 var spear = (phyObject as Spear);
-                                if (spear!=null && 
+                                if (spear!=null && !(spear.thrownBy is Player) &&
                                     (spear.mode != Weapon.Mode.StuckInWall && spear.mode != Weapon.Mode.StuckInCreature && spear.mode != Weapon.Mode.Thrown))
                                 {
                                     if (spear.mode == Weapon.Mode.Carried || spear.mode == Weapon.Mode.OnBack)
@@ -116,18 +121,19 @@ namespace MagneCat.hook
                                             continue;
                                         else
                                             spear.grabbedBy[0].grabber.ReleaseGrasp(spear.grabbedBy[0].grabber.grasps.IndexOf(spear.grabbedBy[0]));
-                                     }
-                                   
+                                    }
 
                                     var spearPos = player.room.GetTilePosition(spear.firstChunk.pos);
                                     var playerPos = player.room.GetTilePosition(player.mainBodyChunk.pos);
+                                    if (!player.room.RayTraceTilesForTerrain(spearPos.x, spearPos.y, playerPos.x, playerPos.y))
+                                        continue;
+
+                                 
                                     if (Custom.DistLess(phyObject.firstChunk.pos, player.mainBodyChunk.pos, 30))
                                     {
                                         StoreSpear(spear);
                                         continue;
                                     }
-                                    if (!player.room.RayTraceTilesForTerrain(spearPos.x, spearPos.y, playerPos.x, playerPos.y))
-                                        continue;
 
                                     if (spear.mode != Weapon.Mode.Free)
                                     {
@@ -148,12 +154,10 @@ namespace MagneCat.hook
                         if (getSpearCounter == 20)
                         {
                             getSpearCounter = 0;
-                            var spear = (Spear)GetSpear();
-                            Debug.Log("Get Spear " + spear == null);
+                            var spear = (Spear)GetSpear(); 
                             if (spear != null)
-                            {
                                 player.SlugcatGrab(spear, player.FreeHand());
-                            }
+                            
                         }
                     }
                     else if (getSpearCounter > 0)
@@ -167,15 +171,49 @@ namespace MagneCat.hook
 
         }
 
- 
+        public void Stun(int st)
+        {
+            Player player;
+            if (!playerRef.TryGetTarget(out player) || spearList.Count == 0)
+                return;
+            SSOracleBehavior? behavior = null;
+            foreach(var a in player.room.physicalObjects)
+                foreach(var b in a)
+                    if(b is Oracle && (b as Oracle).oracleBehavior is SSOracleBehavior)
+                        behavior = (b as Oracle).oracleBehavior as SSOracleBehavior;
+            if (st >= Random.Range(60,80) || (st>=40 && behavior != null && behavior.action == SSOracleBehavior.Action.General_GiveMark))
+            {
+                Spear spear;
+                while((spear = GetSpear())!=null)
+                {
+                    spear.firstChunk.vel = Custom.RNV() * Random.Range(10, 25);
+                    spear.SetRandomSpin();
+                    stunCoolDownCounter = 300;
+                }
+            }
+            else if(st >= Random.Range(10, 30))
+            {
+                int count = Random.Range(2,4);
+                Spear spear;
+                while ((spear = GetSpear()) != null && (count--)!=0)
+                {
+                    spear.firstChunk.vel = Custom.RNV() * Random.Range(10, 25);
+                    spear.SetRandomSpin();
+                    stunCoolDownCounter = st*3;
+                }
+            }
+        }
 
         public FloatingSpearState SpearState { get; private set; }
 
         public WeakReference<Player> playerRef;
+
         public float energy;
-        List<int> spearList;
-        int getSpearCounter = 0;
 
         public FloatingCore floatingCore;
+
+        List<int> spearList;
+        int getSpearCounter = 0;
+        int stunCoolDownCounter = 0;
     }
 }
